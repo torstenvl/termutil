e7afe208b98afa263855bfde710d45ecea22ee6e
Lee Ockert <torstenvl@gmail.com>
2025-03-04 20:41:36 -0500

    More exploration

--- termutil.c
+++ termutil.c
@@ -1,157 +1,130 @@
+// INCLUDES
+#include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
-
-// NEED TO KNOW
-// Character set/code page (incl. UTF-8, UTF-16, etc.)
-// If Unicode, what Unicode version supported
-// Length of various Arabic ligatures
-
-struct TUInfo {
-    short int ucver;
-    short 
-}
+#include <termios.h>
+#include <unistd.h>
+#include <stdbool.h>
 
 
-
-// 	/* Adjust output channel */
-// 	tcgetattr(filedes, &ostate);		/* save old state */
-// 	nstate = ostate;			/* get base of new state */
-// #ifndef cfmakeraw
-// 	nstate.c_iflag &= ~(IMAXBEL|IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL|IXON);
-// 	nstate.c_oflag &= ~OPOST;
-// 	nstate.c_lflag &= ~(ECHO|ECHONL|ICANON|ISIG|IEXTEN);
-// 	nstate.c_cflag &= ~(CSIZE|PARENB);
-// 	nstate.c_cflag |= CS8;
-// #else
-// 	cfmakeraw(&nstate);
-// #endif
-// 	tcsetattr(filedes, TCSADRAIN, &nstate);	/* set mode */
-
-// 	/* Query size of terminal by first trying to position cursor */
-// 	if (write(filedes, query, sizeof(query)) != -1 && poll(&fd, 1, 300) > 0) {
-// 		int row = 0, col = 0;
-
-// 		/* Terminal responds with \e[row;posR */
-// 		if (scanf("\e[%d;%dR", &row, &col) == 2) {
-// 			t.t_nrow = row;
-// 			t.t_ncol = col;
-// 		}
-// 	}
-
-
-#ifndef true
-#define true 1
-#endif
-#ifndef false
-#define false 0
+// DEFINES
+#define CTRL_KEY(k) ((k) & 0x1f)
+#ifndef thread_local
+#define thread_local _Thread_local
 #endif
 
-static int TU_INTERNAL_SysIsUTF8(void) {
-    char *syslang = getenv("LANG");
-    size_t len = strlen(syslang);
-    if (len >= 5) return ( !strcmp("UTF-8", &syslang[len-5]) ) ? true : false;
-    return false;
-}
+#define TU_DUMB          0
+#define TU_CDPGLEGACY    1
+#define TU_UTF8LEGACY    2
+#define TU_UTF8NOLIGS    3
+#define TU_FULLUTF8APPLE 4
+#define TU_FULLUTF8      5
+#define TU_FULLUTF82027  6
 
+static const char *TERMTYPES[] = {
+    "Dumb Terminal",
+    "Legacy Terminal w/ Code Page",
+    "Legacy Terminal w/ UTF-8 Support",
+    "Terminal w/ Basic UTF-8 Support (no grapheme clustering)",
+    "Terminal w/ Full UTF-8 Support (Apple Terminal quirks)",
+    "Terminal w/ Full UTF-8 Support (no Mode 2027 support)",
+    "Terminal w/ Full UTF-8 Support and Mode 2027 support",
+};
 
-
-static int TU_INTERNAL_SSHIsActive(void) {
-    return (getenv("SSH_CLIENT") || getenv("SSH_CONNECTION")) ? true : false;
-}
-
-
-
-static int TU_INTERNAL_TerminalIsLegacy(void) {
-    char *term = getenv("TERM");
-    if (term) {
-        #define strequals(a, b) (!strcmp(a, b))
-        return (strequals(term, "vt100")) ? true :
-            (strequals(term, "vt220"))    ? true :
-            (strequals(term, "cons25"))   ? true :
-            (strequals(term, "rxvt"))     ? true :
-            (strequals(term, "cons25-c")) ? true :
-            (strequals(term, "ansi"))     ? true :
-            (strequals(term, "Eterm"))    ? true :
-            (strequals(term, "dumb"))     ? true :
-            (strequals(term, "kterm"))    ? true :
-            (strequals(term, "cygwin"))   ? true : false;
-        #undef strequals
-    }
-    return false;
-}
-
-
-
-static int TU_UTF8(void) {
-    static _Thread_local int retval = -1;
-    static _Thread_local unsigned long lastcheck = 0;
-    static const int checkinterval = CLOCKS_PER_SEC * 60 * 5;
-
-    if (retval == -1 || clock() > lastcheck + checkinterval) {
-        lastcheck = clock();
-        if (TU_INTERNAL_SSHIsActive())
-            retval = !TU_INTERNAL_TerminalIsLegacy();
-        else
-            retval = TU_INTERNAL_SysIsUTF8();
-    }
-
-    return retval;
-}
-
-
-
-// Some info is at
-//   /etc/terminfo
-//   /usr/share/terminfo
-//   /lib/terminfo
-//   /etc/termcap
-
-// enum TUTermType {
-//     ASCII,
-//     ALACRITTY,
-//     APPLETERM,
-//     GHOSTTY,
-//     GNOME,
-//     ITERM,
-//     KITTY,
-//     VTE,
-//     WEZTERM,
-//     XFCE,
-//     XTERM,
-// }
-// struct TUInfo {
-//     short ucver;
-//     short
-// }
-// int TUInternalCheckAcceptEnv(const char * const var) {
-//     static thread_local int firsttime = 1;
-//     static thread_local char **env = NULL;
-//     FILE *sshd_config;
-//
-//     if (firsttime) {
-//
-//         sshd_config = fopen("/etc/ssh/sshd_config", "rb");
-//         if (!sshd_config) return 0;
-//
-//     }
-// }
-// struct TUInfo TUGetInfo(void) {
-//     bool ssh = (getenv("SSH_CLIENT") || getenv("SSH_CONNECTION")) ? true : false;
-// }
-
-
-
-// int main(void) {
-//     TUInfo = TUGetInfo()
-// }
-
-#ifdef TU_TESTDRIVER
-int main(void) {
-    if (TU_UTF8()) printf("Your system locale is UTF-8.\n");
-    else printf("Your system local is not UTF-8.\n");
+void TUApplyRawMode(bool enable) {
+    static thread_local struct termios rawmode;
+    static thread_local struct termios normalmode;
     
+    // First run
+    if (rawmode.c_iflag == 0) {
+        tcgetattr(STDIN_FILENO, &normalmode);
+        rawmode = normalmode;
+        rawmode.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+        rawmode.c_oflag &= ~(OPOST);
+        rawmode.c_cflag |= (CS8);
+        rawmode.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+        rawmode.c_cc[VMIN] = 0;
+        rawmode.c_cc[VTIME] = 1;
+    }
+    
+    if (enable) tcsetattr(STDIN_FILENO, TCSAFLUSH, &rawmode);
+    else        tcsetattr(STDIN_FILENO, TCSAFLUSH, &normalmode);
+}
+
+size_t TUGetTermTextLength(const char * const text) {
+    static const char * const LINESTART = "\033[0G";
+    static const char * const SAVECURSOR = "\033[s";
+    static const char * const RESTORECURSOR = "\033[u";
+    static const char * const MAKEHIDDEN = "\033[8m";
+    static const char * const UNMAKEHIDDEN = "\033[28m";
+    static const char * const GETCURSORPOS = "\033[6n";
+    size_t row;
+    size_t col;
+
+
+    write(STDOUT_FILENO, LINESTART, strlen(LINESTART));
+    write(STDOUT_FILENO, MAKEHIDDEN, strlen(MAKEHIDDEN));
+    write(STDOUT_FILENO, text, strlen(text));
+    write(STDOUT_FILENO, UNMAKEHIDDEN, strlen(UNMAKEHIDDEN));
+    write(STDOUT_FILENO, GETCURSORPOS, strlen(GETCURSORPOS));
+    write(STDOUT_FILENO, LINESTART, strlen(LINESTART));
+    fflush(stdout);
+
+    scanf("\033[%zu;%zuR", &row, &col);
+
+    return col - 1;    
+}
+
+
+// INIT
+int main() {
+    unsigned char c = '\0';
+    char   buffer[128];
+    size_t length; 
+
+    TUApplyRawMode(true);
+
+    strcpy(buffer, u8"Hello, World!");
+    length = TUGetTermTextLength(buffer);
+    printf("Length of \'%s\' is %zu\r\n", buffer, length);
+
+    strcpy(buffer, u8"﷽           ");
+    length = TUGetTermTextLength(buffer);
+    printf("Length of \'%s\' is %zu\r\n", buffer, length);
+
+    strcpy(buffer, u8"🧑‍🌾");
+    length = TUGetTermTextLength(buffer);
+    printf("Length of \'%s\' is %zu\r\n", buffer, length);
+
+    strcpy(buffer, u8"🧑");
+    length = TUGetTermTextLength(buffer);
+    printf("Length of \'%s\' is %zu\r\n", buffer, length);
+
+    strcpy(buffer, u8"🌾");
+    length = TUGetTermTextLength(buffer);
+    printf("Length of \'%s\' is %zu\r\n", buffer, length);
+    
+
+    while (1) {
+        if (read(STDIN_FILENO, &c, 1) == -1) {
+            switch (errno) {
+                case EAGAIN:
+                    break;
+                default:
+                    perror("read");
+                    exit(EXIT_FAILURE);
+            }
+        }
+        if (c == 0) continue;
+        if (c == CTRL_KEY('q')) break;
+        if (iscntrl(c)) printf("%d\r\n", c);
+        else printf("%d ('%c')\r\n", c, c);
+        c = 0;
+    }
+
+    TUApplyRawMode(false);
+
     return 0;
 }
-#endif
