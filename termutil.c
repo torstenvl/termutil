4783aabbff9af5350def13bfa012b7642f5f89a3
Lee Ockert <torstenvl@gmail.com>
2025-03-12 01:31:11 -0400

    More exploration, undecided on API

--- termutil.c
+++ termutil.c
@@ -1,130 +1,166 @@
-// INCLUDES
-#include <ctype.h>
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 #include <termios.h>
 #include <unistd.h>
-#include <stdbool.h>
 
 
-// DEFINES
-#define CTRL_KEY(k) ((k) & 0x1f)
-#ifndef thread_local
-#define thread_local _Thread_local
+// We use anonymous internal unions and structs to make an efficient TUInfo 
+// structure.  As a result, we need C11 or higher or GNU/Clang extensions.
+#if !(__STDC_VERSION__ >= 201112L)                      && \
+    !(__GNUC__ >= 4 && !defined(__STRICT_ANSI__))       && \
+    !(__clang_major__ >= 2 && !defined(__STRICT_ANSI__)) 
+#error 
+This library requires C11 or higher or GNU/Clang extensions to support anony-\n\
+mous internal unions and structs. Please use a newer compiler or check your\n\
+compiler settings to ensure extensions are enabled.
 #endif
 
-#define TU_DUMB          0
-#define TU_CDPGLEGACY    1
-#define TU_UTF8LEGACY    2
-#define TU_UTF8NOLIGS    3
-#define TU_FULLUTF8APPLE 4
-#define TU_FULLUTF8      5
-#define TU_FULLUTF82027  6
 
-static const char *TERMTYPES[] = {
-    "Dumb Terminal",
-    "Legacy Terminal w/ Code Page",
-    "Legacy Terminal w/ UTF-8 Support",
-    "Terminal w/ Basic UTF-8 Support (no grapheme clustering)",
-    "Terminal w/ Full UTF-8 Support (Apple Terminal quirks)",
-    "Terminal w/ Full UTF-8 Support (no Mode 2027 support)",
-    "Terminal w/ Full UTF-8 Support and Mode 2027 support",
-};
+struct TUInfo {
+    int errorcode; // Cf. errno
+    struct {
+        struct termios origattr;
+        struct termios rawattr;
+        unsigned int init : 1; // Whether our structure is initialized
+        unsigned int raw  : 1; // Whether we're currently in raw mode
+        unsigned int ssh  : 1; // Whether we're working over a SSH connection
+        unsigned int utf8 : 1; // Whether UTF-8 is supported (otherwise ASCII)
+        unsigned int cjk  : 1; // Whether doublewidth CJK chars are supported
+        unsigned int arw  : 1; // Whether arabic ligatures are wide
+        unsigned int em   : 1; // Whether emoji are supported
+        unsigned int emu  : 1; // Whether emoji are unaligned (need forced)
+        unsigned int emj  : 1; // Whether emoji can be joined
+    };
+} ;
 
-void TUApplyRawMode(bool enable) {
-    static thread_local struct termios rawmode;
-    static thread_local struct termios normalmode;
+
+static ssize_t TU_INTERNAL_GetCursorMoveLength(const char * const text);
+
+struct TUInfo TUInitialize(int enablerawmode) {
+    static struct TUInfo T = {};
+
+    #define env(a)      (getenv(a))
+    #define streq(a,b)  (!strcmp(a,b))
+    #define strhas(a,b) (!!strstr(a,b))
     
-    // First run
-    if (rawmode.c_iflag == 0) {
-        tcgetattr(STDIN_FILENO, &normalmode);
-        rawmode = normalmode;
-        rawmode.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-        rawmode.c_oflag &= ~(OPOST);
-        rawmode.c_cflag |= (CS8);
-        rawmode.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-        rawmode.c_cc[VMIN] = 0;
-        rawmode.c_cc[VTIME] = 1;
-    }
-    
-    if (enable) tcsetattr(STDIN_FILENO, TCSAFLUSH, &rawmode);
-    else        tcsetattr(STDIN_FILENO, TCSAFLUSH, &normalmode);
-}
-
-size_t TUGetTermTextLength(const char * const text) {
-    static const char * const LINESTART = "\033[0G";
-    static const char * const SAVECURSOR = "\033[s";
-    static const char * const RESTORECURSOR = "\033[u";
-    static const char * const MAKEHIDDEN = "\033[8m";
-    static const char * const UNMAKEHIDDEN = "\033[28m";
-    static const char * const GETCURSORPOS = "\033[6n";
-    size_t row;
-    size_t col;
-
-
-    write(STDOUT_FILENO, LINESTART, strlen(LINESTART));
-    write(STDOUT_FILENO, MAKEHIDDEN, strlen(MAKEHIDDEN));
-    write(STDOUT_FILENO, text, strlen(text));
-    write(STDOUT_FILENO, UNMAKEHIDDEN, strlen(UNMAKEHIDDEN));
-    write(STDOUT_FILENO, GETCURSORPOS, strlen(GETCURSORPOS));
-    write(STDOUT_FILENO, LINESTART, strlen(LINESTART));
-    fflush(stdout);
-
-    scanf("\033[%zu;%zuR", &row, &col);
-
-    return col - 1;    
-}
-
-
-// INIT
-int main() {
-    unsigned char c = '\0';
-    char   buffer[128];
-    size_t length; 
-
-    TUApplyRawMode(true);
-
-    strcpy(buffer, u8"Hello, World!");
-    length = TUGetTermTextLength(buffer);
-    printf("Length of \'%s\' is %zu\r\n", buffer, length);
-
-    strcpy(buffer, u8"﷽           ");
-    length = TUGetTermTextLength(buffer);
-    printf("Length of \'%s\' is %zu\r\n", buffer, length);
-
-    strcpy(buffer, u8"🧑‍🌾");
-    length = TUGetTermTextLength(buffer);
-    printf("Length of \'%s\' is %zu\r\n", buffer, length);
-
-    strcpy(buffer, u8"🧑");
-    length = TUGetTermTextLength(buffer);
-    printf("Length of \'%s\' is %zu\r\n", buffer, length);
-
-    strcpy(buffer, u8"🌾");
-    length = TUGetTermTextLength(buffer);
-    printf("Length of \'%s\' is %zu\r\n", buffer, length);
-    
-
-    while (1) {
-        if (read(STDIN_FILENO, &c, 1) == -1) {
-            switch (errno) {
-                case EAGAIN:
-                    break;
-                default:
-                    perror("read");
-                    exit(EXIT_FAILURE);
-            }
+    //-------------------------------------------------------------------------
+    //                           TERMINAL MODESETTING
+    //-------------------------------------------------------------------------
+    if (!T.init) {
+        // If we haven't stored the original terminal settings or derived a
+        // raw mode setting yet, let's do that. But first let's ensure we're
+        // in an interactive terminal. 
+        if (!isatty(STDIN_FILENO)) return -ENOTTY;
+        if (tcgetattr(STDIN_FILENO, &T.origattr) == -1) return -ENOTTY;
+        T.rawattr = T.origattr;
+        T.rawattr.c_lflag &= ~(ECHO|ICANON|IEXTEN|ISIG);
+        T.rawattr.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL|IXON);
+        T.rawattr.c_oflag &= ~(OPOST);
+        T.rawattr.c_cflag &= ~(CSIZE|PARENB);
+        T.rawattr.c_cflag |=  (CS8);
+        T.rawattr.c_cc[VMIN]  = 0;
+        T.rawattr.c_cc[VTIME] = 1;
+        // Now, use some heuristics to figure out what our text output looks
+        // like (UTF-8? Emoji behavior? Etc.). 
+        if (!env("SSH_CLIENT")) {
+            if (strhas(env("TERM"), 
         }
-        if (c == 0) continue;
-        if (c == CTRL_KEY('q')) break;
-        if (iscntrl(c)) printf("%d\r\n", c);
-        else printf("%d ('%c')\r\n", c, c);
-        c = 0;
+        T.init = true;
     }
 
-    TUApplyRawMode(false);
+    // Toggle raw mode. This code cannot be run unless the terminal settings
+    // have already been checked and we know STDIN_FILENO is a valid file
+    // descriptor and the terminal is interactive. However, tcsetattr() can
+    // still fail with errno as EINTR or EINVAL. 
+    if (T.init && enablerawmode) {
+        if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &T.rawattr) == -1) {
+            return -errno;
+        }
+        T.raw = true;
+    } else if (T.init && !enablerawmode) {
+        if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &T.origattr) == -1) {
+            return -errno;
+        }
+        T.raw = false;
+    }
+
+
+    //-------------------------------------------------------------------------
+    //                           TERMINAL MODESETTING
+    // Ensure we're in an interactive terminal, get current settings, modify
+    // them to derive a raw mode, and try to set the raw mode.
+    //-------------------------------------------------------------------------
+
+    // Try to get cursor position
+    write(STDOUT_FILENO, "\e[6n", 4); 
+    if (read(STDIN_FILENO, &c, 1) < 1 || 
+        c != '\e' || 
+        (scanf("[%zd;%zdR", &row, &col) != 2)) {
+            // Can't get cursor position :(
+    }
+
+
 
     return 0;
+    #undef streq(a,b) (strcmp(a,b) == 0)
+    #undef strhas(a,b) (strstr(a,b) != NULL)
+}
+
+
+
+static ssize_t TU_INTERNAL_GetCursorMoveLength(const char * const text) {
+    size_t row = 0;
+    size_t col = 0;
+    ssize_t chk;
+    unsigned char c = 0;
+
+    if (!TUTERM.rawmode) {
+        fprintf(stderr, "Invalid use of internal API without Raw Mode enabled.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    write(STDOUT_FILENO, "\e[0G", 4);          // Start of line
+    write(STDOUT_FILENO, "\e[8m", 4);          // Make hidden
+    write(STDOUT_FILENO, text, strlen(text));  // Write text
+    write(STDOUT_FILENO, "\e[28m", 5);         // Undo hidden
+    write(STDOUT_FILENO, "\e[6n", 4);          // Get cursor pos'n
+    write(STDOUT_FILENO, "\e[0G", 4);          // Back to start
+
+    chk = read(STDIN_FILENO, &c, 1);
+    if (chk < 1 || c != '\e') {
+        fprintf(stderr, "Terminal response did not start with ESC: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    chk = scanf("[%zu;%zuR", &row, &col);
+    if (chk != 2) {
+        fprintf(stderr, "Terminal response did not contain cursor position: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    return (col) ? col - 1 : 0;
+}
+
+
+
+
+int main(void) {
+    ssize_t chk;
+    int retval = 0;
+
+    TUInit();
+    TURawMode(true);
+
+    if ((chk = TUGetCursorMoveLength(u8"丟")) < 0) goto ERRORHANDLING;
+    printf("Length of \'%s\' is %zu\r\n", u8"丟", chk);
+    if ((chk = TUGetCursorMoveLength(u8"🧑‍🌾")) < 0) goto ERRORHANDLING;
+    printf("Length of \'%s\' is %zu\r\n", u8"🧑‍🌾", chk);
+    TURawMode(false);
+    return 0;
+
+    ERRORHANDLING:
+    printf("Failed to get cursor move length: %s\r\n", strerror(-chk));
+    return chk;
 }
